2398914c530263d5f8c239ea4e89fc9e97e6ae5d
Robson Locatelli <rlocatelli9@gmail.com>
2023-07-11 17:08:03 -0300

    build(pratic-project-2): created migration

---
+++ pratic-project-2/src/database/migrations/20230711142346_add-type-to-transactions.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.text('type').after('amount').index()
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('type')
+  })
+}
