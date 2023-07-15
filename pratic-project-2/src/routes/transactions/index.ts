cd39af36f2b6f645fba02e2a209332a119495ce2
Robson Locatelli <rlocatelli9@gmail.com>
2023-07-15 09:42:08 -0300

    feat(pratic-project-2): created route for get transactions

--- pratic-project-2/src/routes/transactions/index.ts
+++ pratic-project-2/src/routes/transactions/index.ts
@@ -5,9 +5,24 @@ import { FastifyInstance } from 'fastify'
 import { z as zod } from 'zod'
 
 export async function transactionsRoutes(server: FastifyInstance) {
-  server.get('/', async () => {
+  server.get('/', async (_, response) => {
     const transactions = await knex('transactions').select('*')
-    return transactions
+    return response.status(200).send({
+      transactions,
+      total: transactions.length,
+    })
+  })
+
+  server.get('/:id', async (request, response) => {
+    const getTransactionParamsSchema = zod.object({
+      id: zod.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where({ id }).first()
+
+    return response.status(200).send(transaction)
   })
 
   server.post('/', async (request, response) => {
