71b6ee0ba8052c30ba340216f07e022d345d22ad
Robson Locatelli <rlocatelli9@gmail.com>
2023-07-23 11:01:18 -0300

    test(pratic-project-2): the created test should be able to list all transactions

--- pratic-project-2/test/transactions/transactions.spec.ts
+++ pratic-project-2/test/transactions/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import request from 'supertest'
 
 import { app } from 'app'
@@ -21,5 +21,29 @@ describe('Transactions routes', () => {
         type: 'debit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const transactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 200,
+        type: 'credit',
+      })
+
+    const cookies = transactionResponse.get('Set-Cookie')
+
+    const transactionList = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(transactionList.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 200,
+        type: 'credit',
+      }),
+    ])
   })
 })
